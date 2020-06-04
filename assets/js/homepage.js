39567bc70aee91941eaeb27d52efebc495736858
Chris Hadley <hadleyc15@yahoo.com>
2020-06-04 13:28:03 -0600

    added ability to search by language type

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -25,6 +26,20 @@ var getUserRepos = function (user) {
         });
 };
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
@@ -37,6 +52,16 @@ var formSubmitHandler = function (event) {
         alert("Please enter a GitHub username");
     }
 
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -86,3 +111,4 @@ var displayRepos = function (repos, searchTerm) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
